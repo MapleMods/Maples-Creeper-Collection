377b7561d89c87db8720bc2f4e6cf030bea0425c
MapleMods <250133667+MapleMods@users.noreply.github.com>
2026-03-28 11:54:58 +0100

    Maple's Creeper Collection 1.0 for 1.21.11

---
+++ neoforge/src/main/java/io/github/maplemods/maplescreepercollection/NeoForgeMod.java
@@ -0,0 +1,14 @@
+package io.github.maplemods.maplescreepercollection;
+
+
+import io.github.maplemods.maplescreepercollection.data.Constants;
+import net.neoforged.bus.api.IEventBus;
+import net.neoforged.fml.common.Mod;
+
+@Mod(Constants.MOD_ID)
+public class NeoForgeMod {
+
+    public NeoForgeMod(IEventBus eventBus) {
+        CommonMod.init();
+    }
+}
