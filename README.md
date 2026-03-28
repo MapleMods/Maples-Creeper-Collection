40666d1805ebaa866bf6462104a02c069995526a
MapleMods <250133667+MapleMods@users.noreply.github.com>
2026-03-28 11:54:56 +0100

    Maple's Creeper Collection 1.0 for 1.21.1

---
+++ README.md
@@ -0,0 +1,9 @@
+# Maple's Creeper Collection
+
+Minecraft 1.21.1 | Version 1.0
+
+Download the mod here! :)
+
+CurseForge: https://legacy.curseforge.com/minecraft/mc-mods/maples-creeper-collection
+
+Modrinth: https://modrinth.com/mod/maples-creeper-collection
---
+++ common/src/main/java/io/github/maplemods/maplescreepercollection/data/Constants.java
@@ -0,0 +1,11 @@
+package io.github.maplemods.maplescreepercollection.data;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class Constants {
+	public static final String MOD_ID = "maplescreepercollection";
+	public static final String MOD_NAME = "Maple's Creeper Collection";
+	public static final String MOD_VERSION = "1.0";
+	public static final Logger LOG = LoggerFactory.getLogger(MOD_NAME);
+}
